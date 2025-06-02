99d0c00a3e45c36a39eac7604805677c27b8fbca
btwmeta <137120250+metabtw@users.noreply.github.com>
2025-06-02 23:26:09 +0300

    c#_API

---
+++ 3KatmanAPI/3KatmanDigital_API/Controllers/ProjectRequestController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _3KatmanDigital_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectRequestController : ControllerBase
+    {
+    }
+}
